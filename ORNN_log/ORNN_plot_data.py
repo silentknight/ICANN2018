66a90c0d2643011245f8f03b7af2a8f373c944a8
silentknight <1491628+silentknight@users.noreply.github.com>
2022-02-05 13:40:46 +0530

    New up

--- ORNN_log/ORNN_plot_data.py
+++ ORNN_log/ORNN_plot_data.py
@@ -1,13 +1,8 @@
+#!/usr/bin/env python
+
+import seaborn as sns
+import matplotlib.pyplot as plt
 import numpy as np
-import Gnuplot
-
-g = Gnuplot.Gnuplot()
-g.title("Orthogonal RNN training graph")
-
-g.xlabel("Epoch")
-g.ylabel("Perplexity")
-
-g("set grid")
 
 y1 = np.array([6.51120015417,2.65914440874,2.59454459157,2.58470486614,2.58140266172,2.57990662227,2.57892127583,2.57834224435,2.57783726623,2.57759839973,2.57740764922,2.57717077275,2.57702959255,2.57691047997,2.57682406264,2.57676199094])
 y2 = np.array([6.53032148291,2.64896653676,2.57818493391,2.56855529958,2.56557452115,2.56409838813,2.5633282519,2.56277365255,2.56249119346,2.56217680236,2.56198914228,2.56177684816,2.56163279526,2.56148383059,2.56139645164,2.56131801864])
@@ -15,18 +10,22 @@ y3 = np.array([5.11934803701,2.67442004202,2.63960259999,2.61807523318,2.6019156
 y4 = np.array([4.73292420122,2.69912066855,2.66913336903,2.65452055116,2.63780068143,2.62209454667,2.61135158167,2.60103370173,2.58941784675,2.57642351314,2.56054390583,2.53966555209,2.51975815704,2.50772758118,2.50087876287,2.49645036566])
 y5 = np.array([42.4321422241,30.7374137611,24.6913149474,22.003735207,20.8540602145,20.1897453952,19.7055022284,19.3026203375,18.9443261163,18.6203592587,18.3139203446,18.0198727078,17.7344910266,17.4557096949,17.1805124624,16.9086134704])
 
-x = np.arange (len(y1))
+# red dashes, blue squares and green triangles
+with plt.style.context(('seaborn')):
+    plt.plot(np.arange(1,len(y1)+1), y1, label='Dataset 1 Perplexity')
+    plt.plot(np.arange(1,len(y2)+1), y2, label='Dataset 2 Perplexity')
+    plt.plot(np.arange(1,len(y3)+1), y3, label='Dataset 3 Perplexity')
+    plt.plot(np.arange(1,len(y4)+1), y4, label='Dataset 4 Perplexity')
+    plt.plot(np.arange(1,len(y5)+1), y5, label='PTB Perplexity')
 
-d1 = Gnuplot.Data (x, y1, title="Dataset 1 Perplexity", with_="lines")
-d2 = Gnuplot.Data (x, y2, title="Dataset 2 Perplexity", with_="lines")
-d3 = Gnuplot.Data (x, y3, title="Dataset 3 Perplexity", with_="lines")
-d4 = Gnuplot.Data (x, y4, title="Dataset 4 Perplexity", with_="lines")
-d5 = Gnuplot.Data (x, y5, title="PTB Perplexity", with_="lines linecolor rgb 'black'")
-
-g("set terminal svg")
-g.plot(d1, d2, d3, d4)
-
-# g.hardcopy (filename='perplexity.png', terminal='png')
-g.hardcopy (filename='ornn_perplexity.svg', terminal='svg')
-
-del g
+    plt.tick_params(labelsize='large', width=5)
+    plt.grid(True)
+    #plt.grid(which='major', linestyle='-.', linewidth='0.5', color='grey')
+    #plt.grid(which='minor', linestyle=':', linewidth='0.2', color='grey')
+    ax = plt.axes()
+    ax.set_xlim(1, len(y1))
+    ax.set_xlabel('Number of Epochs', fontsize=15)
+    ax.set_ylabel('Perplexity in bpc', fontsize=15)
+    lgd = ax.legend(loc='upper right', shadow=True, fancybox=True, ncol=1, numpoints=1) 
+    plt.savefig('ornn_perplexity', bbox_extra_artists=(lgd,), bbox_inches='tight')
+    plt.show()
--- RHN_log/RHN_plot_data.py
+++ RHN_log/RHN_plot_data.py
@@ -1,34 +1,31 @@
+#!/usr/bin/env python
+
+import seaborn as sns
+import matplotlib.pyplot as plt
 import numpy as np
-import Gnuplot
 
-g = Gnuplot.Gnuplot()
-g.title("RHN training graph")
+y1 = np.array([1.935,1.856,1.854,1.865,1.829,1.841,1.820,1.804,1.794,1.782])
+y2 = np.array([1.656,1.681,1.669,1.672,1.663])
+y3 = np.array([1.588,1.661,1.633,1.590,1.633,1.644])
+y4 = np.array([1.530,1.592,1.593,1.595,1.601])
+y5 = np.array([3.437,2.812,2.453,2.211,2.016])
 
-g.xlabel("Epoch")
-g.ylabel("Perplexity")
+# red dashes, blue squares and green triangles
+with plt.style.context(('seaborn')):
+    plt.plot(np.arange(1,len(y1)+1), y1, label='Dataset 1 Perplexity')
+    plt.plot(np.arange(1,len(y2)+1), y2, label='Dataset 2 Perplexity')
+    plt.plot(np.arange(1,len(y3)+1), y3, label='Dataset 3 Perplexity')
+    plt.plot(np.arange(1,len(y4)+1), y4, label='Dataset 4 Perplexity')
+    plt.plot(np.arange(1,len(y5)+1), y5, label='PTB Perplexity')
 
-g("set grid")
-
-y1 = np.array([3.821,3.619,3.613,3.642,3.550])
-y2 = np.array([3.150,3.205,3.179,3.183,3.167])
-y3 = np.array([3.007,3.164,3.102,3.011,3.102])
-y4 = np.array([2.893,3.020,3.022,3.024,3.032])
-
-y5 = np.array([3.729,3.366,3.183,3.054,2.979])
-
-x = np.arange (len(y1))
-
-d1 = Gnuplot.Data (x, y1, title="Dataset 1 Training Perplexity", with_="lines")
-d2 = Gnuplot.Data (x, y2, title="Dataset 2 Training Perplexity", with_="lines")
-d3 = Gnuplot.Data (x, y3, title="Dataset 3 Training Perplexity", with_="lines")
-d4 = Gnuplot.Data (x, y4, title="Dataset 4 Training Perplexity", with_="lines")
-d5 = Gnuplot.Data (x, y5, title="Test8 Valid Perplexity", with_="lines linecolor rgb 'black'")
-
-g("set yrange [2:6]")
-g("set terminal svg")
-g.plot(d1, d2, d3, d4, d5)
-
-# g.hardcopy (filename='perplexity.png', terminal='png')
-g.hardcopy (filename='rhn_perplexity.svg', terminal='svg')
-
-del g
+    # plt.tick_params(labelsize='large', width=5)
+    # plt.grid(True)
+    # plt.grid(which='major', linestyle='-.', linewidth='0.5', color='grey')
+    # plt.grid(which='minor', linestyle=':', linewidth='0.2', color='grey')
+    # ax = plt.axes()
+    # ax.set_xlim(1, len(y1))
+    # ax.set_xlabel('Number of Epochs', fontsize=15)
+    # ax.set_ylabel('Perplexity in bpc', fontsize=15)
+    # lgd = ax.legend(loc='upper right', shadow=True, fancybox=True, ncol=1, numpoints=1) 
+    # plt.savefig('rhn_perplexity', bbox_extra_artists=(lgd,), bbox_inches='tight')
+    plt.show()
